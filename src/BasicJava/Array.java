fed60534577e9a31539c924f30a93e34f6e52a38
Andrey <positko.andrei@gmail.com>
2019-08-05 20:34:41 +0300

    BaseArray

---
+++ src/BasicJava/Array.java
@@ -0,0 +1,9 @@
+package BasicJava;
+
+public class Array {
+    int[] arr = {5, 15, 25, 65, 87};
+    boolean[] booleanArray = {true, false};
+    double[] doubleArray = {5.25, 7.854};
+    int[][] array = {{5, 7, 9},{65, 47, 85}};
+
+}
